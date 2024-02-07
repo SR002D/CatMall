2bdf2bb6b447054b5db63cb048007d43db878e06
SR <610311761@qq.com>
2024-02-07 22:03:09 +0800

    更新ignore

---
+++ catmall-product/src/test/java/com/nwafu/catmall/product/CatmallProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nwafu.catmall.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatmallProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ catmall-ware/src/test/java/com/nwafu/catmall/ware/CatmallWareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nwafu.catmall.ware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatmallWareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
