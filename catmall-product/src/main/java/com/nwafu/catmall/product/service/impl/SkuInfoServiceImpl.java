abc5608e2cb2cc2de20ca4d684d966d41077ba62
SR <610311761@qq.com>
2024-03-25 14:17:26 +0800

    商品系统-全部完成

--- catmall-product/src/main/java/com/nwafu/catmall/product/service/impl/SkuInfoServiceImpl.java
+++ catmall-product/src/main/java/com/nwafu/catmall/product/service/impl/SkuInfoServiceImpl.java
@@ -1,5 +1,6 @@
 package com.nwafu.catmall.product.service.impl;
 
+import com.mysql.cj.util.StringUtils;
 import org.springframework.stereotype.Service;
 import java.util.Map;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
@@ -29,6 +30,47 @@ public class SkuInfoServiceImpl extends ServiceImpl<SkuInfoDao, SkuInfoEntity> i
     @Override
     public void saveSkuInfo(SkuInfoEntity skuInfoEntity) {
         this.baseMapper.insert(skuInfoEntity);
+    }
+
+    @Override
+    public PageUtils queryPageByCondition(Map<String, Object> params) {
+        QueryWrapper<SkuInfoEntity> queryWrapper = new QueryWrapper<>();
+
+        String key = (String) params.get("key");
+        if(!StringUtils.isNullOrEmpty(key)){
+            queryWrapper.and(w->{
+                w.eq("sku_id",key).or().like("sku_name",key);
+            });
+        }
+
+        String catelogId = (String) params.get("catelogId");
+        if(!StringUtils.isNullOrEmpty(catelogId) && !catelogId.equals("0")){
+            queryWrapper.eq("catalog_id",catelogId);
+        }
+
+        String brandId = (String) params.get("brandId");
+        if(!StringUtils.isNullOrEmpty(brandId) && !brandId.equals("0")){
+            queryWrapper.eq("brand_id",brandId);
+        }
+
+        String min = (String) params.get("min");
+        if(!StringUtils.isNullOrEmpty(min)){
+            queryWrapper.ge("price",min);
+        }
+
+        String max = (String) params.get("max");
+        if(!StringUtils.isNullOrEmpty(max) && !max.equals("0")){
+            queryWrapper.le("price",max);
+        }
+
+
+        IPage<SkuInfoEntity> page = this.page(
+                new Query<SkuInfoEntity>().getPage(params),
+                queryWrapper
+        );
+
+        return new PageUtils(page);
+
     }
 
 }
