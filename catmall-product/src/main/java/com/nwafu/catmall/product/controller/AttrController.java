ff4c8b7e47e98a1d47629b33978d24851e1523cc
SR <610311761@qq.com>
2024-03-25 21:57:53 +0800

    商品系统-完善

--- catmall-product/src/main/java/com/nwafu/catmall/product/controller/AttrController.java
+++ catmall-product/src/main/java/com/nwafu/catmall/product/controller/AttrController.java
@@ -1,8 +1,11 @@
 package com.nwafu.catmall.product.controller;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
+import com.nwafu.catmall.product.entity.ProductAttrValueEntity;
+import com.nwafu.catmall.product.service.ProductAttrValueService;
 import com.nwafu.catmall.product.vo.AttrRespVo;
 import com.nwafu.catmall.product.vo.AttrVo;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,12 +30,21 @@ public class AttrController {
     @Autowired
     private AttrService attrService;
 
+    @Autowired
+    private ProductAttrValueService productAttrValueService;
+
     @GetMapping("/{attrType}/list/{catelogId}")
     public R baseQueryList(@RequestParam Map<String, Object> params,
                           @PathVariable("catelogId") Long catelogId,
                            @PathVariable("attrType")String type){
         PageUtils page =attrService.queryBaseAttrPage(params,catelogId,type);
         return R.ok().put("page", page);
+    }
+
+    @GetMapping("/base/listforspu/{spuId}")
+    public R baseAttrListforspu(@PathVariable("spuId") Long spuId){
+        List<ProductAttrValueEntity> entites = productAttrValueService.baseAttrListforspu(spuId);
+        return R.ok().put("data",entites);
     }
 
     /**
@@ -78,6 +90,13 @@ public class AttrController {
     public R update(@RequestBody AttrVo attr){
 		attrService.updateAttr(attr);
 
+        return R.ok();
+    }
+
+    @PostMapping("/update/{spuId}")
+    public R updateSpuAttr(@PathVariable("spuId") Long spuId,
+                     @RequestBody List<ProductAttrValueEntity> entities){
+        productAttrValueService.updateSpuAttr(spuId,entities);
         return R.ok();
     }
 
